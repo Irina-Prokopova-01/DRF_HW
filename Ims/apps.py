c7348e9f9fb0a18ab9e24c26c5b892a3094be4da
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-12-18 14:07:34 +0300

    add viewset, Serializers.py, generic

--- Ims/apps.py
+++ Ims/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ImsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Ims'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Ims"
--- Ims/models.py
+++ Ims/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Course(models.Model):
+    """Модель курса"""
+    title = models.CharField(
+        max_length=150, verbose_name="Название", help_text="Введите название курса"
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        blank=True,
+        null=True,
+        help_text="Введите описание категории",
+    )
+    image = models.ImageField(
+        upload_to="images/",
+        verbose_name="Изображение",
+        blank=True,
+        null=True,
+        help_text="Загрузите фотографию курса",
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "курс"
+        verbose_name_plural = "курсы"
+
+
+class Lesson(models.Model):
+    """Модель урока"""
+    title = models.CharField(
+        max_length=300,
+        verbose_name="Название урока",
+        help_text="Введите название урока"
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        blank=True,
+        null=True,
+        help_text="Введите описание урока",
+    )
+    preview = models.ImageField(
+        upload_to="lms/previews",
+        verbose_name="Превью",
+        blank=True,
+        null=True,
+        help_text="Загрузите изображение",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name="Курс",
+        help_text="Выберите курс",
+        null=True,
+        blank=True,
+    )
+    owner = models.ForeignKey(
+        User,
+        verbose_name="Владелец курса",
+        on_delete=models.CASCADE,
+        help_text="Введите владельца курса",
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
