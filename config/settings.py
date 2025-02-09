c4c3d376fad0a59143071c396344c51774cd8ca7
Irina Prokopova <irina-prokopova.style@yandex.ru>
2025-02-09 16:15:57 +0300

    add ci.yml

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
@@ -157,4 +158,12 @@ EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
