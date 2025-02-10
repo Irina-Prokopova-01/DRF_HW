29eadf4d054f151d299c53d3e03573ca9df291e9
Irina Prokopova <irina-prokopova.style@yandex.ru>
2025-02-10 11:25:21 +0300

    corect 14

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,13 @@ load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Путь к файлу .env
+env_file_path = os.path.join(os.path.dirname(__file__), '.env')
+
+# Проверка существования файла .env
+if not os.path.exists(env_file_path):
+    raise FileNotFoundError(f"Файл .env не найден по пути: {env_file_path}. Пожалуйста, создайте его.")
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
